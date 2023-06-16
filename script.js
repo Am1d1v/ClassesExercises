1b079979e55fa693c6bed8c1056222302c59541c
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-17 00:01:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+const Book = function(title, author){
+    this.title = title;
+    this.author = author;
+};
+
+Book.prototype.read = function(){
+    this.isRead = true;
+};
+
+class BookClass {
+    constructor(title, author){
+        this.title = title;
+        this.author = author;
+        this.isRead = false;
+    };
+
+    read(){
+        this.isRead = false;
+    }
+};
+
