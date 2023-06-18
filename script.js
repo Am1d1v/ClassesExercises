3de5abe6cd503d4cf643299c5afa5f1210c95a3c
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-18 19:05:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -153,3 +153,54 @@ class Car {
 const car = new Car();
 console.log(car);
 console.log(car.test());
+
+//===============================
+
+class User{
+    #login;
+    #_password;
+    constructor(login, password){
+        this.#login = login;
+        this.#password = password;
+
+    }
+
+    set #password(pass){
+        this.#_password = pass.split(' ').reverse().join();
+    }
+
+    get #password(){
+        return this.#_password.split(' ').reverse().join();
+    }
+
+    get login(){
+        return this.#login;
+    }
+
+    checkPassword(pass){
+        return this.#password == pass;
+    }
+
+    changePassword(oldPass, newPass){
+        if(!this.checkPassword(oldPass)){
+            return  false;
+        }
+        
+        this.#password = newPass;
+        return true;
+    }
+}
+
+const user = new User("a@a","123");
+console.log(user);
+console.log(user.login);
+
+/*
+
+user.login = "aaaaaaaaaaa@a.com";
+console.log(user.login);
+
+*/
+
+console.log(user.checkPassword("123"));
+console.log(user.checkPassword("223")); //false password
